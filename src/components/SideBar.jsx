f989f0080a1e920f3f1f6461707462aaeb87153a
shruti-doon <shruti23cc12@gmail.com>
2024-08-19 05:57:36 +0530

    update2

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar() {
 
   return (
     <div className="relative">
-      {/* Hamburger Icon for small screens */}
+     
       <button
         className="top-0 left-1 lg:hidden text-gray-600 z-10"
         onClick={toggleSidebar}
@@ -21,13 +21,13 @@ export default function Sidebar() {
         </span>
       </button>
 
-      {/* Sidebar */}
+    
       <aside
         className={`fixed inset-y-0 left-0 bg-slate-200 border p-4 sm:p-5 md:w-64 lg:w-72 xl:w-80 transition-transform transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } lg:translate-x-0 lg:static lg:transform-none z-10`}
       >
-        {/* Close Button inside the Sidebar */}
+       
         <button
           className="absolute top-4 right-4 lg:hidden text-gray-600 p-2"
           onClick={closeSidebar}
@@ -36,7 +36,7 @@ export default function Sidebar() {
         </button>
 
         <div className="flex flex-col h-full">
-          <div className="flex-1 overflow-y-auto">
+          <div className="flex-1 overflow-y-auto mt-2 mr-1">
             <CreateEventButton />
             <SmallCalendar />
             <Labels />
